714a812c0dd3da8c062f07f3b84b687bb1dd780e
LulDrako <karimfeki2004@gmail.com>
2024-08-31 15:36:06 +0200

    Update social Discord link to use the correct URL

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Smooth scroll for navigation
 document.querySelectorAll('nav ul li a').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
@@ -8,7 +7,6 @@ document.querySelectorAll('nav ul li a').forEach(anchor => {
     });
 });
 
-// Project tabs functionality
 const tabs = document.querySelectorAll('.project-tabs li');
 const projects = document.querySelectorAll('.project-detail');
 const tabsContainer = document.querySelector('.project-tabs');
@@ -17,7 +15,6 @@ tabs.forEach(tab => {
     tab.addEventListener('click', function() {
         const isActive = this.classList.contains('active');
         
-        // Reset all tabs and project details
         tabs.forEach(item => item.classList.remove('active'));
         projects.forEach(project => project.classList.remove('active'));
         
@@ -26,18 +23,16 @@ tabs.forEach(tab => {
             const projectToShow = document.getElementById(this.dataset.project);
             projectToShow.classList.add('active');
             
-            // Center+ the selected tab
             tabsContainer.classList.add('centered');
             tabs.forEach(item => {
                 if (!item.classList.contains('active')) {
-                    item.style.display = 'none'; // Hide other tabs
+                    item.style.display = 'none';
                 }
             });
         } else {
-            // Show all tabs if deselected
             tabsContainer.classList.remove('centered');
             tabs.forEach(item => {
-                item.style.display = 'block'; // Show all tabs
+                item.style.display = 'block';
             });
         }
     });
@@ -47,8 +42,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const skills = document.querySelectorAll('.skill');
 
     const observerOptions = {
-        root: null,  // Uses the viewport as the root
-        threshold: 0.1  // Triggers when at least 10% of the element is visible
+        root: null,
+        threshold: 0.1
     };
 
     const observer = new IntersectionObserver((entries) => {
@@ -56,15 +51,12 @@ document.addEventListener('DOMContentLoaded', () => {
             if (entry.isIntersecting) {
                 entry.target.classList.add('skills-appear');
             } else {
-                entry.target.classList.remove('skills-appear'); // Remove the class when the element is out of view
+                entry.target.classList.remove('skills-appear');
             }
         });
     }, observerOptions);
 
     skills.forEach(skill => {
-        observer.observe(skill);  // Observe each skill for visibility changes
+        observer.observe(skill);
     });
 });
-
-
-
